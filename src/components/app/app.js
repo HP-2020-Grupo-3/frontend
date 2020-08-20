3ed330b20d8176c9162a508fa7af52d1b7128a96
twissell- <dmaggioesne@gmail.com>
2020-08-19 21:44:20 -0300

    basic project layout

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import './app.css';
+import Navegacion from '../navbar/navbar'
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Navegacion />
+        <Row className="justify-content-md-center">
+          <Col xs lg="12">
+            <h1>Veni de Mary</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col xs lg="3">
+            menu
+          </Col>
+          <Col xs lg="9">
+            pantalla de cada menu
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
